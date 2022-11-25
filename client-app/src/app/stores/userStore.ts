d0f20c225b8628be743dc09564c2a788b97ff151
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-25 17:53:30 +0000

    fb starter

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -61,4 +61,13 @@ export default class UserStore {
   setDisplayName = (name: string) => {
     if (this.user) this.user.displayName = name;
   };
+
+  facebookLogin = () => {
+    window.FB.login(
+      (response) => {
+        console.log(response);
+      },
+      { scope: 'public_profile,email' }
+    );
+  };
 }
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
+import {
+  Button,
+  Container,
+  Divider,
+  Header,
+  Image,
+  Segment,
+} from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
@@ -43,6 +50,16 @@ export default observer(function HomePage() {
             >
               Register
             </Button>
+            <Divider horizontal inverted>
+              Or
+            </Divider>
+            <Button
+              size='huge'
+              inverted
+              color='facebook'
+              content='Login with Facebook'
+              onClick={userStore.facebookLogin}
+            />
           </Fragment>
         )}
       </Container>
